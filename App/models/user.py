5855b2f318a316497adfdb2faec8e286b86492cc
Clicked1 <cupckes422@gmail.com>
2022-04-03 14:33:17 +0000

    User Model Update

--- App/models/user.py
+++ App/models/user.py
@@ -3,8 +3,11 @@ from App.database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username =  db.Column(db.String(80),unique=True,nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+
 
     def __init__(self, username, password):
         self.username = username
