b1ececdb500a6eff2b7885fab5fc3f09aa40f153
Clicked1 <cupckes422@gmail.com>
2022-04-03 16:32:09 +0000

    login/signup pgae and routes

--- App/views/api.py
+++ App/views/api.py
@@ -4,4 +4,8 @@ api_views = Blueprint('api_views', __name__, template_folder='../templates')
 
 @api_views.route('/', methods=['GET'])
 def get_api_docs():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@api_views.route('/signup', methods=['GET'])
+def get__signup_page():
+    return render_template('signup.html')
